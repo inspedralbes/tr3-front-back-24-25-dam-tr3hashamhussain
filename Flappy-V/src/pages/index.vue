6cfe66a6b2747d043802db8e32a6927b737df895
a23hashusraf <a23hashusraf@inspedralbes.cat>
2025-03-14 10:05:05 +0100

    cracion del vuetify

---
+++ Flappy-V/src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <ImageUpload />
+  </v-container>
+</template>
+
+<script>
+import ImageUpload from '@/components/ImageUpload.vue';
+
+export default {
+  components: {
+    ImageUpload,
+  },
+};
+</script>
